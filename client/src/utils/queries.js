ecde4ba5eabb34c4415d08a02da2a6d1db38ec40
agvan <agvandyke25@gmail.com>
2022-09-17 15:40:49 -0500

    added GET_ME to the queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,22 @@
 // imports gql from apollo/client
 import { gql } from "@apollo/client";
+
+
+export const GET_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}
+`;
