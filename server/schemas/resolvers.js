20cf5addf8a2d60a4fd9bc5e4564652d0a888695
agvan <agvandyke25@gmail.com>
2022-09-17 12:27:43 -0500

    set up resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,3 +4,9 @@ const {AuthenticationError} = require('apollo-server-express');
 const {User, Book} = require('../models');
 // imports the sign token function from auth
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+};
+
+module.exports = resolvers;
