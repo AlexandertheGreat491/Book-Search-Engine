cba18ee7285cd44e8eee2b1a9d2f33d31b3be396
agvan <agvandyke25@gmail.com>
2022-09-17 16:50:25 -0500

    imported useMutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import {useMutation} from '@apollo/client';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
