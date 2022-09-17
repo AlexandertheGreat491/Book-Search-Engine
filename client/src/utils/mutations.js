e429f82655b5a7451992aed37025cb82ea5984ed
agvan <agvandyke25@gmail.com>
2022-09-17 16:29:25 -0500

    corrected the mutations and started updated LoginForm

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,21 +1,63 @@
 // imports the gql from apollo/client
-import {gql} from '@apollo/client';
+import { gql } from "@apollo/client";
 
-export const GET_ME = gql`
-{
-    me {
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
         _id
         username
-        email
-        bookCount
-        savedBooks {
-            bookId
-            authors
-            description
-            title
-            image
-            link
-        }
+      }
     }
-}
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $passwrod) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: saveBookInput) {
+    saveBook(bootData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        title
+        authors
+        image
+        description
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
 `;
