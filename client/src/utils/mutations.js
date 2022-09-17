658808f4ba5827ea21010bde64a7f1a0cfbb7842
agvan <agvandyke25@gmail.com>
2022-09-17 15:44:32 -0500

    added GET_ME to mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,21 @@
 // imports the gql from apollo/client
 import {gql} from '@apollo/client';
+
+export const GET_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}
+`;
