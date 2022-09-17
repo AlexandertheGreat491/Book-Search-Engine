42e5c9f4590ff537a0849a9745ce1b2cbf18faf5
agvan <agvandyke25@gmail.com>
2022-09-17 12:30:45 -0500

    added Query to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,18 @@ const {User, Book} = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
+Query: {
+    me: async (parent, args, context) => {
+        if (context.user) {
+            const userData = await User.findOne({_id: context.user._id})
+            .select('-_v -password')
 
+            return userData;
+        }
+
+        throw new AuthenticationError('You are not logged in!');
+    }
+},
 };
 
 module.exports = resolvers;
