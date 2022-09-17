fb437cac6562ba010a83d0db67ef3684f44586c1
agvan <agvandyke25@gmail.com>
2022-09-17 14:47:59 -0500

    cleaned up the code

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,34 @@
 // imports react
-import React from 'react';
+import React from "react";
 // imports BrowserRouter from react-router-dom
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // imports the SearchBooks page
-import SearchBooks from './pages/SearchBooks';
+import SearchBooks from "./pages/SearchBooks";
 // imports the SavedBooks page
-import SavedBooks from './pages/SavedBooks';
+import SavedBooks from "./pages/SavedBooks";
 // imports the NavBar component
-import Navbar from './components/Navbar';
-// imports ApolloProvide , ApolloClient, InMemoryCache, createHttpLink form apollo-client
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Navbar from "./components/Navbar";
+// imports ApolloProvider , ApolloClient, InMemoryCache, createHttpLink form apollo-client
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
 function App() {
   return (
     <Router>
       <>
         <Navbar />
-          <Routes>
-            <Route 
-              path='/' 
-              element={<SearchBooks />} 
-            />
-            <Route 
-              path='/saved' 
-              element={<SavedBooks />} 
-            />
-            <Route 
-              path='*'
-              element={<h1 className='display-2'>Wrong page!</h1>} 
-            />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<SearchBooks />} />
+          <Route path="/saved" element={<SavedBooks />} />
+          <Route
+            path="*"
+            element={<h1 className="display-2">Wrong page!</h1>}
+          />
+        </Routes>
       </>
     </Router>
   );
