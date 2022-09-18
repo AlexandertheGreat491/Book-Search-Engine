e7b86e41c13698296a39b7baa250d41ca3614a85
agvan <agvandyke25@gmail.com>
2022-09-18 18:41:37 -0500

    changed the connection logic

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 // exports the connection logic so that it can be utilized by the application
 module.exports = mongoose.connection;
