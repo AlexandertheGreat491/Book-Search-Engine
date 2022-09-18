d245e796440894d99a446628299e51810a65081d
agvan <agvandyke25@gmail.com>
2022-09-18 14:27:19 -0500

    changed connection logic again, still really trying to solve deployment problems

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 // connects to my database on MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 // exports the connection logic so that it can be utilized by the application
 module.exports = mongoose.connection;
