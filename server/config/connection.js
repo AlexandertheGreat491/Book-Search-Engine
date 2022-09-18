aa2412e44e4d7a946c1163f1af28146081e5a03e
agvan <agvandyke25@gmail.com>
2022-09-18 12:25:18 -0500

    added two comments

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-
+// connects to my database on MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
-
+// exports the connection logic so that it can be utilized by the application
 module.exports = mongoose.connection;
