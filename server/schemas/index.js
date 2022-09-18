de7508c3c0407b68adc8464fc0cc3cd977c3b57c
agvan <agvandyke25@gmail.com>
2022-09-18 12:44:58 -0500

    added one comment

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,4 @@
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
-
+// exports the typeDefs and resolvers so that they can be used by the server
 module.exports = {typeDefs, resolvers};
