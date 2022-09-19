cd45caaff0c89dc8ce559386f1ad303acda758e1
agvan <agvandyke25@gmail.com>
2022-09-19 17:04:18 -0500

    changed it back

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose');
 // connects to my database on MongoDB Atlas
-let db = process.env.db || "mongodb+srv://Insomnia123:Winston1235@cluster0.c9efx.mongodb.net/book-search?retryWrites=true&w=majority"
-mongoose.connect(db), {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-}
-    .then(_ => console.log("Connected to database."));
+});
 // exports the connection logic so that it can be utilized by the application
 module.exports = mongoose.connection;
-// used code from https://stackoverflow.com/questions/68896073/unable-to-connect-to-mongodb-database-in-heroku-even-when-pasting-db-connection
